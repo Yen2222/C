a6b3bd1a4745b1732540cac6adf73d920d4923bc
Yen2222 <127270568+Yen2222@users.noreply.github.com>
2023-06-15 17:03:02 +0900

    Add files via upload

---
+++ 13-9.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+void main()
+{
+	struct student {
+		char name[10];
+		int age;
+	};
+
+	int count;
+
+	printf("입력할 학생 수 : ");
+	scanf("%d", &count);
+
+	struct student s;
+	struct student *p;
+
+	p = (struct student*) malloc(sizeof(struct student) * count);
+
+	for (int i=0; i<count; i++)
+	{
+		printf("이름과 나이 입력 : ");
+		scanf("%s %d", (p + i)->name, &(p + i)->age);
+	}	
+
+	printf("\n-- 학생 명단 --\n");
+	for (int i=0; i<count; i++) 
+	{
+		printf("이름:%s , 나이:%d \n", (p + i)->name, (p + i)->age);
+	}
+}
